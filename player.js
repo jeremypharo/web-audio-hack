b5bf95756e933746e4e946d6a8871ac869947ba8
Todd Mazierski <toddmazierski@gmail.com>
2013-12-07 13:37:24 -0500

    Change player to use an audio tag.

--- player.js
+++ player.js
@@ -1,9 +1,9 @@
-function playSound(buffer) {
-  var source = context.createBufferSource(); // creates a sound source
-  source.buffer = buffer;                    // tell the source which sound to play
-  source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-  source.start(0);                           // play the source now
-                                             // note: on older systems, may have to use deprecated noteOn(time);
-}
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-loadDavidRocking('full_techno_jacket_103_924.mp3', playSound);
+function playAudioID(id) {
+  var context = new AudioContext;
+  var element = document.getElementById(id);
+  var source = context.createMediaElementSource(element);
+  source.connect(context.destination);
+  element.play();
+}
